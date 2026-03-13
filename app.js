007a62872016d9a8a186a24791923c1e32268884
falconA7 <kaweco17@gmail.com>
2026-03-13 23:52:20 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+
+const readline = require('readline');
+
+const api_key = `${process.env.API_KEY}`;
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function get_weather_by_zip_code(formatted_zip){
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${formatted_zip},JP&appid=${api_key}&lang=ja`;
+
+    axios.get(url)
+        .then(function(response){
+            const weather_data = response.data;
+            console.log(weather_data);
+        })
+        .catch(function(error){
+            console.log('エラーが発生しました。郵便番号が正しいか確認してください。');
+        })
+}
+
+rl.question('郵便番号を入力してください(半角数字7桁):',
+    (zip) => {
+    if (!zip || isNaN(zip) || zip.length !== 7) {
+        console.log('有効な7桁の郵便番号を入力してください。');
+    } else {
+        const formatted_zip = `${zip.slice(0,3)}-${zip.slice(3)}`;
+        console.log(`入力された郵便番号: ${formatted_zip}`);
+        get_weather_by_zip_code(formatted_zip)
+    }
+    rl.close();
+});
+
+
+
+
+
+
+
+
+
+
